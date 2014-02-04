155acafba5f12badeb2337bd46b251e58ced4f0c
Michael <accounts@michiwend.com>
2014-02-05 00:04:32 +0100

    rundumleuchte done

--- willie/modules/labping.py
+++ willie/modules/labping.py
@@ -1,4 +1,8 @@
 # -*- coding: utf8 -*-
+
+from __future__ import unicode_literals
+import serial
+
 """
 
 the labping module for OpenLab Augsburg
@@ -7,13 +11,41 @@ the labping module for OpenLab Augsburg
 from willie.module import commands
 
 
-@commands('.labping')
+serial_dev = None
+ltime = 5
+
+def re_connect():
+    global serial_dev
+    
+
+    try:
+        serial_dev.close() # close an existing connection
+    except:
+        pass
+
+    try:
+        serial_dev = serial.Serial('/dev/lights', 9600, timeout=1)
+    except:
+        raise Exception('fatal: rundumleuchte connected?')
+
+
+def setup(bot):
+    re_connect()
+
+
+@commands('labping')
 def ping_openlab(bot, trigger):
     """
     heinrich lässt die Rundumleuchte im OpeLab aufleuchten
     """
-    #rundumleuchte aktivieren
-    sleep(5)
-    #rundumleuchte deaktivieren
 
+    global ltime
 
+    try:
+        serial_dev.write(str(ltime))
+    except serial.SerialException:
+        bot.say(trigger.nick + ': Keine Verbindung zur Rundumleuchte :( Versuche es nochmal.')
+        re_connect()
+        return
+
+    bot.say('Rundumleuchte wurde für ' + str(ltime) + ' Sekunden aktiviert')
