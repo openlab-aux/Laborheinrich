9e42a305d8c88036a369e8a884e73b62384ca416
Michael <accounts@michiwend.com>
2014-02-06 01:05:28 +0100

    now it werks

--- willie/modules/labping.py
+++ willie/modules/labping.py
@@ -34,12 +34,12 @@ class Rundumleuchte:
         except:
             pass
 
-        connect(Rundumleuchte.dev_file)
+        Rundumleuchte.connect(Rundumleuchte.dev_file)
 
     @staticmethod
     def light_up():
         try:
-            serial_dev.write(str(Rundumleuchte.interval))
+            serial_dev.write(str(Rundumleuchte.lightup_interval))
         except serial.SerialException:
             return False
 
@@ -51,24 +51,26 @@ def setup(bot):
 
         Rundumleuchte.connect(bot.config.rundumleuchte.device)
 
-        if bot.config.has_option('rundumleuchte', 'interval'):
-            Rundumleuchte.interval = bot.config.rundumleuchte.interval
+        if bot.config.has_option('rundumleuchte', 'lightup_interval'):
+            Rundumleuchte.light_up_interval = bot.config.rundumleuchte.lightup_interval
         else:
-            Rundumleuchte.interval = 5
+            Rundumleuchte.light_up_interval = 5
     else:
-        raise ConfigurationError('labping not configured')
+        raise ConfigurationError('labping module not configured')
 
 
 @commands('labping')
-def labping(bot):
+def labping(bot, trigger):
     """
     heinrich lässt die Rundumleuchte im OpeLab aufleuchten
     """
     # do it twice if first try fails
     for i in range(2):
         if Rundumleuchte.light_up():
-            bot.say('Rundumleuchte wurde für ' + str(Rundumleuchte.interval) + ' Sekunden aktiviert!')
+            bot.say('Rundumleuchte wurde für ' + str(Rundumleuchte.light_up_interval) + ' Sekunden aktiviert!')
             return
         if i == 0:
             bot.say(trigger.nick + ': Konnte Rundumleuchte nicht aktivieren, ich versuch´s noch einmal...')
             Rundumleuchte.re_connect()
+        else:
+            bot.say(trigger.nick + ': Nein, nichts zu machen :(')
