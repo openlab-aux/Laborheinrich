3c4de45972bbe980b4acc745334dcd9ec3e67c46
Johannes Fürmann <johannes@weltraumpflege.org>
2014-02-10 16:47:53 +0100

    adds tumblr module

---
+++ willie/modules/tumblr.py
@@ -0,0 +1,36 @@
+# -*- coding: utf8 -*-
+
+"""
+tumblr module for heinrich
+"""
+
+from __future__ import unicode_literals
+
+from willie.module import commands
+
+from pytumblr import TumblrRestClient
+
+class Tumblr:
+    api = None
+
+def setup(bot):
+    if bot.config.has_option('tumblr', 'consumer_key') and bot.config.has_option('tumblr', 'consumer_secret') and bot.config.has_option('tumblr', 'oauth_token') and bot.config.has_option('tumblr', 'oauth_secret'):
+        Tumblr.api = TumblrRestClient(bot.config.tumblr.consumer_key,
+                                      bot.config.tumblr.consumer_secret,
+                                      bot.config.tumblr.oauth_token,
+                                      bot.config.tumblr.oauth_secret)
+
+    else:
+        raise Exception('tumblr module not configured')
+
+
+@commands('shit')
+def post(bot, trigger):
+    if trigger.admin:
+        quote = " ".join(trigger.args[1].split(" ")[1:])
+        Tumblr.api.create_quote('shitopenlabsays',
+                                type="quote",
+                                quote=quote)
+        bot.say("Posted quote \"%s\" to http://shitopenlabsays.tumblr.com" % quote)
+    else:
+        bot.say("Nope, you're not allowed to post.")
