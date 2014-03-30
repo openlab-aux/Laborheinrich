a567c4b5e24e2f83e68204c4fb5f6e305b5947a0
Michael Wendland <accounts@michiwend.com>
2014-03-30 13:28:11 +0200

    adds try statements to avoid uncontrolled behavior

--- willie/modules/labstatus.py
+++ willie/modules/labstatus.py
@@ -33,24 +33,39 @@ class LabAPIHandler:
         self.__url = url
 
     def update_data(self):
-        http_obj = urllib.urlopen(self.__url)
-        json_data = http_obj.read()
-        self.__values = json.loads(json_data)
-        http_obj.close()
+        try:
+            http_obj = urllib.urlopen(self.__url)
+            json_data = http_obj.read()
+            self.__values = json.loads(json_data)
+            http_obj.close()
+            return True
+        except IOError:
+            return False
 
     def get_lab_state(self):
-        return self.__values['open']
+        try:
+            return self.__values['open']
+        except (ValueError, KeyError):
+            return 'failed'
+        ''' TODO:
+            handle exceptions in functions '''
 
     def get_active_clients(self):
-        clients_value = self.__values['status'].split()[0]
+        try:
+            clients_value = self.__values['status'].split()[0]
 
-        if( not clients_value.isnumeric() ):
-            return 0
-        else:
-            return int(clients_value)
+            if( not clients_value.isnumeric() ):
+                return 0
+            else:
+                return int(clients_value)
+        except (ValueError, KeyError):
+            return -1
 
     def get_last_change(self):
-        return self.__values['lastchange']
+        try:
+            return self.__values['lastchange']
+        except (ValueError, KeyError):
+            return -1
 
 
 def uptime(timestamp1, timestamp2):
@@ -83,27 +98,34 @@ def uptime(timestamp1, timestamp2):
 @willie.module.interval(LURK_INTERVAL)
 def lurk(bot):
 
+    print('lurink...')
 
     handler = LabAPIHandler(bot.config.labstatus.api_url)
-    handler.update_data()
+    
+    if handler.update_data():
 
         lab_is_open  = handler.get_lab_state()
         lab_was_open = bot.memory['lab_was_open']
 
-    if( lab_is_open and not lab_was_open ):   # state changed to 'open'
-        status_str = 'geöffnet!'
-    elif( not lab_is_open and lab_was_open ): # state changed to 'closed'
-        status_str = 'geschlossen.'
-    else:                                     # nothing happend
-        return
+        status_str = ''
 
-    topic = string.replace(bot.config.labstatus.topic_draft, '$STATUS', status_str)
+        if( lab_is_open and not lab_was_open ):   # state changed to 'open'
+            status_str = 'geöffnet!'
+        elif( not lab_is_open and lab_was_open ): # state changed to 'closed'
+            status_str = 'geschlossen.'
+        else:                                     # nothing happend
+            return
 
-    for channel in bot.channels:
-        bot.write(('TOPIC', channel + ' :' + string.replace(topic, '$CHANNEL', channel)))
-        bot.msg(channel, 'NEUER LAB-STATUS: ' + status_str)
+        topic = string.replace(bot.config.labstatus.topic_draft, '$STATUS', status_str)
+        
+        for channel in bot.channels:
+            bot.write(('TOPIC', channel + ' :' + string.replace(topic, '$CHANNEL', channel)))
+            bot.msg(channel, '+++ NEUER LAB-STATUS: ' + status_str + ' +++')
 
         bot.memory['lab_was_open'] = lab_is_open
+
+    else:
+        print('API call failed');
 
 
 @commands('status')
@@ -113,10 +135,13 @@ def print_status(bot, trigger):
     """
 
     handler = LabAPIHandler(bot.config.labstatus.api_url)
-    handler.update_data()
+    if handler.update_data():
 
-    if( handler.get_lab_state() ):
-        bot.say('Lab-Status: geöffnet. | aktive Geräte: ' + str(handler.get_active_clients())
-                )#+ ' | Geöffnet: ' + uptime(handler.get_last_change(), time.time()))
+        if( handler.get_lab_state() ):
+            bot.say('Lab-Status: geöffnet. | aktive Geräte: ' + str(handler.get_active_clients())
+                    )#+ ' | Geöffnet: ' + uptime(handler.get_last_change(), time.time()))
+        else:
+            bot.say('Lab-Status: geschlossen.')
+        
     else:
-        bot.say('Lab-Status: geschlossen.')
+        bot.say('API-Call nicht möglich')
